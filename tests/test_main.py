410b09f79b5d4d10c4ff997dbdf47f08457514dd
alex <alex@example.com>
2021-12-01 07:35:32 +0100

    day 1
    also extracted python input file generator helper

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import uuid
 
 import pytest
 
@@ -10,12 +11,16 @@ def sut_dir():
     return os.path.dirname("%s/../" % test_dir)
 
 
-def test_demo2020(tmp_path, sut_dir):
-    sample = [1721, 979, 366, 299, 675, 1456]
-    input_text = "\n".join(map(str, sample)) + "\n"
-    input_file_path = "%s/input.txt" % tmp_path
+def input_file(folder, *args):
+    input_text = "\n".join(map(str, args)) + "\n"
+    input_file_path = "%s/%s.txt" % (folder, uuid.uuid1())
     with open(input_file_path, "w") as f:
         f.write(input_text)
+    return input_file_path
+
+
+def test_demo2020(tmp_path, sut_dir):
+    input_file_path = input_file(tmp_path, 1721, 979, 366, 299, 675, 1456)
 
     process = subprocess.Popen(
         ["racket", "%s/demo2020/program.rkt" % sut_dir, input_file_path],
@@ -25,3 +30,28 @@ def test_demo2020(tmp_path, sut_dir):
 
     output = process.communicate()[0].decode("utf-8").strip()
     assert output == "514579"
+
+
+def test_1(tmp_path, sut_dir):
+    input_file_path = input_file(
+        tmp_path,
+        199,
+        200,
+        208,
+        210,
+        200,
+        207,
+        240,
+        269,
+        260,
+        263,
+    )
+
+    process = subprocess.Popen(
+        ["racket", "%s/1/program.rkt" % sut_dir, input_file_path],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+
+    output = process.communicate()[0].decode("utf-8").strip()
+    assert output == "7"
