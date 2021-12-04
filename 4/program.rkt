487dbe1a5d041e1e1d6a6dea581138f672c78516
alex <alex@example.com>
2021-12-04 15:26:17 +0100

    day 4 - wow this getting hard!
    I had to create two tests because I ended up with an `or` in my
    code... so had to make sure I checked both matching rows and matching
    columns.
    
    the smartest shortcut I took today was realising that by transposing
    the matrix, I could reuse the existing bingo card checker
    
    this still took me a few hours
    I am starting to think that maybe I am _not_ a "10x" programmer

---
+++ 4/program.rkt
@@ -0,0 +1,95 @@
+#lang racket
+
+(require racket/cmdline)
+
+;; let's not convert the strings to numbers
+;; until we are ready to add up the value of the winning card
+
+;; since I already know the array size, I don't need all the info in the file
+;; either the blank lines or the number lines
+;; either the newlines or the number of columns
+;; so let's see what is easier
+(define array-size 5)
+(define file-name (vector-ref (current-command-line-arguments) 0))
+
+(define (comma-split str)
+  (string-split str ","))
+
+(define (group size lines [progress '()])
+  (if (= (length lines) 0) ;; so we might get an error if there is a mismatch
+      progress
+      (group size
+             (drop lines size)
+             (append (list (take lines size)) progress))))
+
+(define (read-matrix lines)
+  (map string-split lines))
+
+(define (non-empty line)
+  (> (string-length line) 0))
+
+;; don't know how to use struct so just using a cons of the moves and card
+(define (read-data-file path dim)
+  (let ([relevant-lines (filter non-empty (file->lines path))])
+    (cons (comma-split (car relevant-lines))
+          (map read-matrix (group dim (cdr relevant-lines))))))
+
+(define (row-check row moves)
+  (= 0 (length (filter (lambda (x) (not (member x moves))) row))))
+
+;; filter and check the length instead of recursion and exit when found
+;; how do the pros do it?
+(define (partial-card-check card moves)
+  (> (length (filter (lambda (x) (row-check x moves)) card)) 0))
+
+;; this trick is really coming in handy!
+(define (flip card)
+  (apply map list card))
+
+;; this is the first example of something that will not be verified by turning
+;; the example into an automated test because in the example it is a row that
+;; is found and so the column will never be found. even if we measured test
+;; coverage this issue would no be found because the flipping still happens
+;; on every non winning card but we never find a matching column
+;; to solve this I might get the tests to invert the input but that would
+;; mean rewriting the whole parsing procedure in python
+;; or I could just depend on my manual repl tests but I might want to rewrite
+;; this in future as I get better at LISP so a working test would be handy
+(define (card-check card moves)
+  (or (partial-card-check card moves)
+      (partial-card-check (flip card) moves)))
+
+;; be careful - the value changes depending when the game ends
+(define (card-value card moves)
+  (* (string->number (last moves))
+     (apply + (map string->number
+                   (filter (lambda (x) (not (member x moves)))
+                           (apply append card))))))
+
+(define (check-and-score card moves)
+  (if (card-check card moves)
+      (card-value card moves)
+      0))
+
+;; this will total all the scores instead of filtering
+;; so if there are ever two winners at the same time, instead of an error
+;; we'll just get the wrong answer and have no idea why
+(define (check-all-cards cards moves)
+  (apply + (map (lambda (x) (check-and-score x moves)) cards)))
+
+;; let's try passing the index instead of splitting up the list this time
+;; I tried doing it with a past moves and a future moves argument but since
+;; I couldn't decide which list the current move should be in
+;; so good opportunity to try this, which is less efficient because it has to
+;; traverse the list an extra time every time
+(define (calculate cards all-moves [cursor 1]) ;; start with 1 instead of 0!!
+  (let* ([moves (take all-moves cursor)]
+         [result (check-all-cards cards moves)])
+    (if (> result 0)
+        result
+        (calculate cards all-moves (+ 1 cursor)))))
+
+(let ([input (read-data-file file-name array-size)])
+  (display (calculate (cdr input) (car input))))
+
+(display "\n")
--- tests/test_main.py
+++ tests/test_main.py
@@ -6,13 +6,26 @@ import pytest
 
 
 @pytest.fixture
-def sut_dir():
-    test_dir = os.path.dirname(os.path.realpath(__file__))
+def test_dir():
+    return os.path.dirname(os.path.realpath(__file__))
+
+
+@pytest.fixture
+def sut_dir(test_dir):
     return os.path.dirname("%s/../" % test_dir)
+
+
+@pytest.fixture
+def data_dir(test_dir):
+    return "%s/data" % test_dir
 
 
 def input_file(folder, *args):
     input_text = "\n".join(map(str, args)) + "\n"
+    return raw_input_file(folder, input_text)
+
+
+def raw_input_file(folder, input_text):
     input_file_path = "%s/%s.txt" % (folder, uuid.uuid1())
     with open(input_file_path, "w") as f:
         f.write(input_text)
@@ -97,3 +110,72 @@ def test_3(tmp_path, sut_dir):
 
     output = get_output("%s/3/program.rkt" % sut_dir, input_file_path)
     assert output == "198"
+
+
+def test_4(data_dir, sut_dir):
+    # stop generating the file - the sample data are getting harder to turn
+    # into a python list before turning back into a text file
+    # easier to skip the middle man now
+    # and that means we can load the test input in the repl
+    input_file_path = "%s/4.txt" % data_dir
+
+    output = get_output("%s/4/program.rkt" % sut_dir, input_file_path)
+    assert output == "4512"
+
+
+def test_4_flipped(data_dir, sut_dir, tmp_path):
+    # since their example only deals with rows, I need to flip the whole
+    # problem in python to make sure that it'll work for matching columns
+    # and to do that I have to recreate all the array loading logic here
+    original_input_file_path = "%s/4.txt" % data_dir
+    with open(original_input_file_path) as f:
+        original_content = f.read()
+
+    # split it on the blank lines
+    # in the implementation we depended on counting the lines
+    # having filtered out the line breaks
+    chunks = original_content.strip().split("\n\n")
+
+    adjusted_content = chunks[0] + "\n\n"
+
+    # quickly test the helpers we are gonna use
+    assert trim_matrix_str(chunks[1]) == format_matrix(parse_matrix(chunks[1]))
+    for chunk in chunks[1:]:
+        matrix = parse_matrix(chunk)
+        print(matrix)
+        # print(list(zip*matrix))
+        adjusted_content += format_matrix(zip(*matrix)) + "\n\n"
+
+    print(adjusted_content)
+
+    input_file_path = raw_input_file(tmp_path, adjusted_content)
+
+    output = get_output("%s/4/program.rkt" % sut_dir, input_file_path)
+    assert output == "4512"
+
+
+def parse_matrix(matrix_string):
+    # not using any filter or map in these tests
+    # trying to make them as non functional as possible
+    matrix = []
+    for line in matrix_string.split("\n"):
+        matrix.append(line.replace("  ", " ").strip().split(" "))
+    return matrix
+
+
+def format_matrix(matrix):
+    result = []
+    for line in matrix:
+        result.append(" ".join(line))
+    return "\n".join(result)
+
+
+def trim_matrix_str(matrix_string):
+    old_lines = matrix_string.split("\n")
+    new_lines = []
+    for line in old_lines:
+        new_line = line
+        while "  " in new_line:
+            new_line = new_line.replace("  ", " ")
+        new_lines.append(new_line.strip())
+    return "\n".join(new_lines)
