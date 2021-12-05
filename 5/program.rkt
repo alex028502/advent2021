0bf9f822bb8e71343750035dc25844b1e9d71782
alex <alex@example.com>
2021-12-05 10:26:21 +0100

    remove from github actions and explain in comment

--- 5/program.rkt
+++ 5/program.rkt
@@ -2,6 +2,21 @@
 
 (require racket/cmdline)
 
+;; on the real data set, this program ran for 55 minutes on my raspberry pi 4
+;; with no result before I stopped it
+;; on github actions, it took 4 1/2 minutes
+;; I tried running it in parts against the real data set, and even the stage
+;; that expands each item into a list of points seemed to take a while
+;; but that might have just been from printing them out
+;; If I had to try to make it faster I would start by grouping all the points
+;; into columns or rows, and then adding up each group, so each point only
+;; needed to be compared to like 1000 other points instead of like 1000000
+;; The sorting function could just be called 'classify' and then could be tuned
+;; I would start by classifying into columns, but as long as the classification
+;; formula is deterministic, I think that you could do anything, even make each
+;; point its own list, and see what could get the answer in a reasonable amount
+;; of time
+
 (define (parse-move str)
   (map parse-vector (map string-trim (arrow-split str))))
 
