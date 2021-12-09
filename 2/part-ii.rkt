8da8e99f5f6772f089c5589497c295ab0053eaf4
Alex <alex@example.com>
2021-12-09 22:09:06 +0100

    find a place in day to to try out foldl
    looked through my different recursive functions
    most of them stop when the result reaches a certain state
    not when the input list is empty - so this was a good choice
    except that I didn't want to rename all the variables and there doesn't
    seem to be an easy way to destructure a list (or I haven't come across
    it yet) like python's
    or all the pattern matching stuff in the other functional languages
    so I used a lambda in a really creative way
    
    unfortunately a comment I wrote while figuring out what is and isn't a
    tail call had to be deleted because it no longer applies

--- 2/part-ii.rkt
+++ 2/part-ii.rkt
@@ -6,24 +6,22 @@
 
 (define input (map string-split raw-input))
 
-(define (follow-instructions moves [aim 0] [depth 0] [distance 0])
-  (if (= 0 (length moves))
-      (list aim depth distance)
-      (let ([command (car (car moves))]
-            [mag (string->number (last (car moves)))])
-        (if (string=? "forward" command)
-            ;; I don't know what tail call optimisation means by the "last"
-            ;; thing - are two possibilities in an if both last? or just the
-            ;; one listed last?
-            (follow-instructions (cdr moves)
-                                 aim
-                                 (+ depth (* mag aim))
-                                 (+ distance mag))
-            (follow-instructions (cdr moves)
-                                 (+ aim
-                                    (* mag (if (string=? "up" command) -1 1)))
-                                 depth
-                                 distance)))))
+(define (follow-instructions moves)
+  (foldl (lambda (move result)
+           (apply (lambda (aim depth distance) ; really to destructure list
+                    (let ([command (car move)]
+                          [mag (string->number (last move))])
+                      (if (string=? "forward" command)
+                          (list aim
+                                (+ depth (* mag aim))
+                                (+ distance mag))
+                          (list (+ aim (* mag
+                                          (if (string=? "up" command) -1 1)))
+                                depth
+                                distance))))
+                  result))
+         '(0 0 0)
+         moves))
 
 (display (apply * (cdr (follow-instructions input))))
 
---
+++ 2/readme.txt
@@ -0,0 +1,4 @@
+went back here days later to turn the recursive definition in part-ii.rkt into
+foldl since it was the best candidate I could find.  I did something
+interesting with apply lambda to avoid renaming too many variables
+
