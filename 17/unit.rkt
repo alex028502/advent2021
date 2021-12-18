9d4fc5731e96d499ec1a402808fa2b572a707bce
alex <alex@example.com>
2021-12-18 14:49:33 +0100

    wip return path as well as apex so we can plot what happened to debug

--- 17/unit.rkt
+++ 17/unit.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
+(provide find-best-apex-for-vx)
 (provide check-position)
 (provide check-trajectory)
 (provide />)
@@ -10,17 +11,44 @@
 
 |#
 
-(define (check-trajectory x0 x1 y0 y1 v [pos '(0 0)] [apex 0])
-  (let ([result (apply check-position x0 x1 y0 y1 pos)]
-        [acme (max (last pos) apex)]) ;; instead of "new-apex"
+;;(define (find-trajectory-with-style x0 x1 y0 y1)
+
+;; do we need to worry about y less than zero?
+;; if we need it, than the best we can do is apex 0
+;; which means we don't need to worry about short vs unlucky results
+;; at least not for this stage - even though check-position tells us
+
+(define (find-best-apex-for-vx x0 x1 y0 y1 vx [vy 0] [record 0])
+  (let ([apex (check-trajectory x0 x1 y0 y1 (list vx vy))])
+    (if (or (equal? apex "L") (> vy (* 10 x1)))
+        record
+        (find-best-apex-for-vx x0
+                               x1
+                               y0
+                               y1
+                               vx
+                               (add1 vy)
+                               (max record (if (number? apex) apex 0))))))
+
+;; (define (find-best-apex-for-vy x0 x1 y0 y1 vy [vx 1] [record 0])
+;;   (let ([apex (check-trajectory x0 x1 y0 y1 (list vx vy))])
+;;     (if (equal? apex "L")
+;;         record
+;;         (find-best-apex-for-vx x0 x1 y0 y1 vy (add1 vx) (max record
+;;                                                              (if (number? apex)
+;;                                                                  apex
+;;                                                                  0))))))
+
+(define (check-trajectory x0 x1 y0 y1 v [path '((0 0))] [apex 0])
+  (let* ([pos (car path)]
+         [result (apply check-position x0 x1 y0 y1 pos)]
+         [acme (max (last pos) apex)]) ;; instead of "new-apex"
     (if result
-        (if (equal? result "T") acme result)
-        (apply check-trajectory
-               x0
-               x1
-               y0
-               y1
-               (append (next-v-pos v pos) (list acme))))))
+        (list (if (equal? result "T") acme result) path)
+        (let* ([next-v-pos-tmp (next-v-pos v pos)]
+               [next-v (car next-v-pos-tmp)]
+               [next-pos (last next-v-pos-tmp)])
+          (check-trajectory x0 x1 y0 y1 next-v (cons next-pos path) acme)))))
 
 (define (next-v-pos v pos)
   (list (list (max 0 (sub1 (car v))) ; we never shoot backwards
