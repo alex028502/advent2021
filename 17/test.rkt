ce7df29f0624f73b7248a6a1fb02b174a0a79357
alex <alex@example.com>
2021-12-18 11:23:54 +0100

    wip

---
+++ 17/test.rkt
@@ -0,0 +1,68 @@
+#lang racket
+
+(require rackunit)
+
+(require "./unit.rkt")
+
+#|
+
+there are nine zones - but some can be grouped
+take a look at sample.txt
+
+S - short
+L - long
+T - on target
+. - don't know yet
+U - unlucky
+
+if it hits an unlucky zone I can't use that value, but I can't stop sweeping
+either - just skip to the next one
+
+y to list-ref conversion
+ 1   0
+ 0   1
+-1   2
+-2   3
+list-ref is (- 1 y)
+|#
+
+;; modify function to give X at orgin to make sure origin of test data is
+;; lined up correctly
+(let* ([sample '("........LLLLL" ;;  1 row 
+                 "X.......LLLLL" ;;  0 row
+                 "........LLLLL" ;; -1 row
+                 "........LLLLL" ;; -2
+                 "....TTTTUUUUU" ;; -3
+                 "....TTTTUUUUU" ;; -4
+                 "....TTTTUUUUU" ;; -5
+                 "SSSSUUUUUUUUU" ;; -6
+                 "SSSSUUUUUUUUU" ;; -7 ;; (- 1 h)
+                 "SSSSUUUUUUUUU")] ;; -8 ;; (- 2 h)
+       [dot->false (λ (c) (if (equal? c ".") #f c))]
+       [get-expected-answer (λ (x y) (/> sample
+                                          (curryr list-ref (- 1 y))
+                                          (curryr substring x (add1 x))
+                                          dot->false))]
+       [sut (curry check-position 4 7 -5 -3)]
+       [sutx (λ (x y) (if (and (= x 0) (= y 0)) "X" (sut x y)))]
+       [w (string-length (car sample))]
+       [h (length sample)]
+       [pts (cartesian-product (range 0 w) (range (- 2 h) 2))])
+  (begin (check-equal? 1 1)
+         (check-equal? (length pts) (* 13 10))
+         (for-each (λ (coordinates)
+                     (check-equal? (apply sutx coordinates)
+                                   (apply get-expected-answer coordinates)
+                                   coordinates))
+                   pts)))
+
+
+;; example from question
+(let ([sut (curry check-trajectory 20 30 -10 -5)])
+  (check-equal? (sut '(7 2)) 3)
+  (check-equal? (sut '(6 3)) 6)
+  (check-equal? (sut '(9 0)) 0)
+  (check-equal? (sut '(17 4)) "U")
+  (check-equal? (sut '(6 9)) 45)
+  (check-equal? (sut '(0 -1)) "S") ;; they didn't give an example for this
+  (check-equal? (sut '(100 100)) "L")) ;; or this
---
+++ 17/unit.rkt
@@ -0,0 +1,37 @@
+#lang racket
+
+(provide check-position)
+(provide check-trajectory)
+(provide />)
+
+#|
+
+(define (optimise x0 x1 y0 y1)
+
+|#
+
+
+(define (check-trajectory x0 x1 y0 y1 v [pos '(0 0)] [apex 0])
+  (let ([result (apply check-position x0 x1 y0 y1 pos)]
+        [acme (max (last pos) apex)]) ;; instead of "new-apex"
+    (if result
+        (if (equal? result "T") acme result)
+        (apply check-trajectory x0 x1 y0 y1 (append (next-v-pos v pos)
+                                                    (list acme))))))
+
+(define (next-v-pos v pos)
+  (list (apply map + v pos)
+        (list (max 0 (sub1 (car v))) ; we never shoot backwards
+              (sub1 0))))
+
+;; see diagram in test to find out what this means
+(define (check-position x0 x1 y0 y1 x y)
+  (cond
+   [(and (<= x x1) (>= x x0) (<= y y1) (>= y y0)) "T"]
+   [(and (< x x0) (< y y0)) "S"]
+   [(and (> x x1) (> y y1)) "L"]
+   [(or (< x x0) (> y y1)) #f] ; false for asking "do we know the answer yet?"
+   [else "U"]))
+
+(define (/> . args)
+  ((apply compose (reverse (cdr args))) (car args)))
