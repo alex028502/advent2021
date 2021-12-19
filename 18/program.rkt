2dcd565083237cb30b727866873a27b791329045
alex <alex@example.com>
2021-12-19 22:53:39 +0100

    day 18 part ii - let's see how long production takes to give me an answer

--- 18/program.rkt
+++ 18/program.rkt
@@ -9,3 +9,20 @@
              snailfish-magnitude))
 
 (display "\n")
+
+;; part ii
+;; 100P2 is like 10k (because it is almost the same thing as 100x100)
+;; and part i did 100 calculations in like a second
+;; so this shouldn't take more than a couple minutes?
+;; and if some of that second was startup time.. ever better
+
+(/> (current-command-line-arguments)
+    (curryr vector-ref 0)
+    file->lines
+    (λ (lines) (cartesian-product lines lines))
+    (curry filter (λ (perm) (not (equal? (car perm) (last perm)))))
+    (curry map (curry apply snailfish-add))
+    (curry map snailfish-magnitude)
+    (curry apply max))
+
+(display "\n")
