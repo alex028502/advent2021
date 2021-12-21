20064f997da1c67702eaeb21461e2f6b3658a2b2
Alex <alex@example.com>
2021-12-21 16:45:43 +0100

    day 21 part ii
    (cherry picked from commit f01dc0b9956553808f113bcf5fede17239f8fd89)
    
    oops - I broken day 21 part i when I did day 21 part ii
    
    (cherry picked from commit 294849bfbfb14aa7b9d485463fe531d91dedc136)

--- 21/module.rkt
+++ 21/module.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 (provide main)
-(provide roll)
+(provide practice-roll)
 (provide roll-once)
 (provide position-+)
 (provide />)
@@ -16,26 +16,27 @@
 ;;          0
 ;;          (range 10000)))
 
-(define (main . start-squares)
-  (play (map string->number start-squares)
-        (make-list (length start-squares) 0)
-        0))
+(define (main . args)
+  (let ([start-squares (filter identity (map string->number args))])
+    (if (member "--practice" args)
+        (practice start-squares (make-list (length start-squares) 0) 0)
+        (play start-squares (make-list (length start-squares) 0) 0))))
 
-(define (play positions points rolls)
+(define (practice positions points rolls)
   (if (> (length (filter (curryr >= 1000) points)) 0)
       (* (apply min points) rolls)
       (let* ([player-idx (modulo (/ rolls n) 2)]
              [new-space (position-+ (list-ref positions player-idx)
-                                    (roll rolls))])
-        (play (list-set positions player-idx new-space)
-              (list-update points player-idx (curryr + new-space))
-              (+ rolls n)))))
+                                    (practice-roll rolls))])
+        (practice (list-set positions player-idx new-space)
+                  (list-update points player-idx (curryr + new-space))
+                  (+ rolls n)))))
 
 ;; this is a perfect case for unit testing
 (define (position-+ pos spaces)
   (/> pos sub1 (curry + spaces) (curryr modulo 10) add1))
 
-(define (roll rolls)
+(define (practice-roll rolls)
   (/> rolls
       (curry make-list n)
       (curry map + (range n))
@@ -48,3 +49,55 @@
 
 (define (/> . args)
   ((apply compose (reverse (cdr args))) (car args)))
+
+;; --- part ii ---
+
+;; I have left this unfinished.  It should get the max of the two numbers
+;; '(106768284484217 63041043110262)
+;; except it takes 11 minutes
+;; so I don't want to run it over and over until I get it working
+;; I could lower the winning score and fix it... but I actually got the
+;; correct answer already, so I'm just gonna leave this one unfinished
+(define (play positions points rolls)
+  (let ([winners (map (curryr >= 21) points)])
+    (if (> (length (filter identity winners)) 0)
+        (map boolean->number winners)
+        (let* ([player-idx (modulo (/ rolls n) 2)]
+               [space (list-ref positions player-idx)])
+          (/> (map
+               (λ (total-frequency)
+                 (vec-*
+                  (last total-frequency)
+                  (let ([new-space (position-+ space (car total-frequency))])
+                    (play (list-set positions player-idx new-space)
+                          (list-update points player-idx (curry + new-space))
+                          (+ rolls n)))))
+               possibilities)
+              (curry apply vec-+))))))
+
+(define (boolean->number b)
+  (if b 1 0))
+
+;; vector addition
+;; borowed from yesterday
+(define (vec-+ . pts)
+  (apply map + pts))
+
+;; vector multiplication (not the datatype vector - it's for lists)
+(define (vec-* n pt)
+  (map (curry * n) pt))
+
+;; the roll count doesn't change the output
+;; let's calculate this one instead of calculating it millions of times
+;; every turn splits into seven possibilities with different weights
+;; > possibilities
+;; '((3 1) (4 3) (5 6) (6 7) (7 6) (8 3) (9 1))
+(define possibilities
+  (filter (λ (x) (> (last x) 0))
+          (map list
+               (range (add1 (* 3 n)))
+               (foldl (λ (next acc) (list-update acc next add1))
+                      (make-list (add1 (* 3 n)) 0)
+                      (map (curry apply +)
+                           (apply cartesian-product
+                                  (make-list 3 (range 1 (add1 n)))))))))
--- 21/test.rkt
+++ 21/test.rkt
@@ -4,7 +4,7 @@
 
 (require "./module.rkt")
 
-(check-equal? (main "4" "8") 739785)
+(check-equal? (main "4" "8" "--practice") 739785)
 
 (check-equal? (roll-once 0) 1)
 (check-equal? (roll-once 1) 2)
@@ -14,12 +14,14 @@
 
 ;; unfortunately no examples for the die turning over but hopefully I can rely
 ;; on the test above
-(check-equal? (roll 0) (+ 1 2 3))
-(check-equal? (roll 3) (+ 4 5 6))
-(check-equal? (roll 6) (+ 7 8 9))
-(check-equal? (roll 9) (+ 10 11 12))
+(check-equal? (practice-roll 0) (+ 1 2 3))
+(check-equal? (practice-roll 3) (+ 4 5 6))
+(check-equal? (practice-roll 6) (+ 7 8 9))
+(check-equal? (practice-roll 9) (+ 10 11 12))
 
 (check-equal? (position-+ 3 3) 6)
 (check-equal? (position-+ 7 (+ 2 2 1)) 2)
 (check-equal? (position-+ 10 1) 1)
 (check-equal? (position-+ 9 1) 10)
+
+;; (check-equal? (main "4" "8") 444356092776315)
