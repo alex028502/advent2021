9bad817e66db113d941ca94645204db4f8e84793
alex <alex@example.com>
2021-12-22 07:52:58 +0100

    day 22 part i - slow
    test took 13 seconds on my raspberry pi 4
    I'll let ci calculate the real answer before I submit it.
    If they ask me what I think they are gonna ask me in part ii, this way
    it's gonna work!

---
+++ 22/test.rkt
@@ -0,0 +1,26 @@
+#lang racket
+
+(require rackunit)
+
+(require "./module.rkt")
+
+(check-equal? (parse-rule "on x=10..12,y=10..12,z=10..12")
+              '(#t (10 12) (10 12) (10 12)))
+(check-equal? (parse-rule "off x=1..2,y=-10..-2,z=3..-60")
+              '(#f (1 2) (-10 -2) (3 -60)))
+
+(check-equal? (in 2 '(1 3)) #t)
+(check-equal? (in 2 '(-1 1)) #f)
+(check-equal? (in 2 '(-1 2)) #t)
+(check-equal? (in 2 '(1 3)) #t)
+(check-equal? (in -2 '(1 3)) #f)
+(check-equal? (in -2 '(-5 -2)) #t)
+(check-equal? (in -2 '(-5 -1)) #t)
+(check-equal? (in -2 '(-5 -3)) #f)
+
+(check-equal? (rule-in-range '(#f (1 2) (-10 -2) (3 -40))) #t)
+(check-equal? (rule-in-range '(#f (1 2) (-10 -2) (3 -60))) #f)
+(check-equal? (rule-in-range '(#t (1 2) (-10 -2) (3000 -60))) #f)
+
+(check-equal? (in3d '(1 2 3) '((1 2) (-20 -12) (-40 3))) #f)
+(check-equal? (in3d '(1 2 3) '((1 2) (-2 4) (-40 3))) #t)
