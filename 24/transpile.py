7457a26debd495ac7f7803d44d6260a799ca73fa
Alex <alex@example.com>
2021-12-27 12:12:57 +0100

    Revert "got back to a c function for each operation"
    This reverts commit d09a0cf8e98cc96b552fbcf2c52e2dbeffd25e39.

--- 24/transpile.py
+++ 24/transpile.py
@@ -2,11 +2,11 @@ import sys
 
 # originally defined the library in C to make it easier to translate each line
 # but then changed my mind to make the assembly a little easier to read
-print("int mul(int a, int b) { return a * b; }")
-print("int add(int a, int b) { return a + b; }")
-print("int div(int a, int b) { return a / b; }")
-print("int mod(int a, int b) { return a % b; }")
-print("int eql(int a, int b) { return (a == b) ? 1 : 0; }")
+# print("int mul(int a, int b) { return a * b; }")
+# print("int add(int a, int b) { return a + b; }")
+# print("int div(int a, int b) { return a / b; }")
+# print("int mod(int a, int b) { return a % b; }")
+# print("int eql(int a, int b) { return (a == b) ? 1 : 0; }")
 
 print("int main(int argc, char * argv[]) {")
 print("  int w = 0; int x = 0; int y = 0; int z = 0;")
@@ -29,11 +29,11 @@ with open(sys.argv[1]) as file:
             continue
 
         cmd, var, val = tokens
-        # if cmd == "eql":
-        #     print("  %s = (%s == %s) ? 1 : 0;" % (var, var, val))
-        #     continue
-        # print("  %s = %s %s %s;" % (var, var, ops[cmd], val))
-        print("  %s = %s(%s, %s);" % (var, cmd, var, val))
+        if cmd == "eql":
+            print("  %s = (%s == %s) ? 1 : 0;" % (var, var, val))
+            continue
+        print("  %s = %s %s %s;" % (var, var, ops[cmd], val))
+        # print("  %s = %s(%s, %s);" % (var, cmd, var, val))
 
 # also tried changing to return z == 0; to see if that made a difference but
 # it didn't, and this one can be tested a little easier with the test program
